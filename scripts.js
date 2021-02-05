d50ca2019bca9766354d16d85c2584b7ebf529bc
lisalisabobisa <lisamabeles@gmail.com>
2021-02-05 10:54:26 -0800

    init commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+/*
+    App: First JavaScript Application
+    Author: Lisa Abeles
+    Date: 01/04/2021
+
+*/
+
+
+// allow the user to enter thier guess
+let num = prompt('Guess a number between 1 and 10');
+// check to see if the number is a winner
+if (num == 5) {
+    // if it is, they got it right
+     alert('Your guessed the right number!');
+    }   else {
+    // if it isn't, they got it wrong    
+    alert('You guessed the wrong number!');
+    }
